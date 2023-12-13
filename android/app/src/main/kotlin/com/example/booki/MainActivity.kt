f3effc5e77c40eca1cc3da02ced8c791da519102
yul <yul@yuls-MacBook-Pro.local>
2023-12-13 22:36:13 +0900

    init

---
+++ android/app/src/main/kotlin/com/example/booki/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.booki
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
+import 'package:intl/intl.dart';
+
+void main() {
+  runApp(const MaterialApp(debugShowCheckedModeBanner: false, home: MyApp()));
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  DateTime today = DateTime.now();
+  void _onDaySelected(DateTime day, DateTime focusedDay) {
+    setState(() {
+      today = day;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: content(),
+    );
+  }
+
+  Widget content() {
+    return Column(
+      children: [
+        const SizedBox(height: 70),
+        Container(
+            child: TableCalendar(
+                headerStyle: const HeaderStyle(
+                  formatButtonVisible: false,
+                  titleCentered: true,
+                ),
+                availableGestures: AvailableGestures.all,
+                selectedDayPredicate: (day) => isSameDay(day, today),
+                focusedDay: today,
+                firstDay: DateTime.utc(2010, 10, 01),
+                lastDay: DateTime.utc(2030, 12, 30),
+                onDaySelected: _onDaySelected)),
+      ],
+    );
+  }
+}
